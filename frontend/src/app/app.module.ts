4ac25b5599048621272d06157b4c2d50e13f3ba3
charles github <carlos.sigua@gmail.com>
2021-03-13 07:54:50 -0500

    initial commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Rellena} from './components/rellena/rellena.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Rellena,
-    AppHeaderComponent
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
